ef1c8b782d8bbae2bff0a7262e2d0672c98de173
lyoshi <380036805@qq.com>
2018-06-03 23:19:07 +0800

    backup

---
+++ ArcLength/PlotModel.m
@@ -0,0 +1,7 @@
+function PlotModel()
+global Node
+x=Node(:,1);
+y=Node(:,2);
+plot(x,y);
+daspect([1 1 1]);
+end
--- ArcLength/main.m
+++ ArcLength/main.m
@@ -2,7 +2,7 @@ clc;clear;
 BuildModel();
 expectU=-2;
 record=SolveModel(expectU);
-global Node Element Material BC1 NF TotalU InterF_elem InterF ExterF dU
+global Node Element Material BC1 NF TotalU InterF_elem InterF ExterF dU r_total lambda_total
 x=record(:,1)*(-1);
 y=record(:,2);
 plot(x,y);
